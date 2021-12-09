7d737a24508d5fc95945bf0427e639c53f8d8055
SolomonRicky2002 <yizhen.c02@gmail.com>
2021-12-09 23:11:10 +0800

    Minor changes

--- custom/DIR-842/diy-part2.sh
+++ custom/DIR-842/diy-part2.sh
@@ -86,6 +86,9 @@ rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
 git clone -b master --depth 1 https://github.com/brvphoenix/wrtbwmon.git package/new/wrtbwmon
 git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git package/new/luci-app-wrtbwmon
 
+# Hostname
+sed -i "s|system.@system[-1].hostname='OpenWrt'|system.@system[-1].hostname='DIR-842'|g" package/base-files/files/bin/config_generate
+
 # Timezone and Zonename
 sed -i "s|system.@system\\[-1\\].timezone='UTC'|system.@system\\[-1\\].timezone='<+8>-8'\\
                 set system.@system[-1].zonename='Asia/Kuala Lumpur'|g" package/base-files/files/bin/config_generate
--- custom/Xiaomi-AX3600/diy-part2.sh
+++ custom/Xiaomi-AX3600/diy-part2.sh
@@ -1,3 +1,11 @@
-sed -i 's/system.@system[-1].timezone='UTC'/system.@system[-1].zonename='Asia/Kuala Lumpur'/g' package/base-files/files/bin/config_generate
-git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git package/luci-app-wrtbwmon
-git clone https://github.com/brvphoenix/wrtbwmon.git package/wrtbwmon
+# Bandwidth Monitor
+rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
+git clone -b master --depth 1 https://github.com/brvphoenix/wrtbwmon.git package/new/wrtbwmon
+git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.git package/new/luci-app-wrtbwmon
+
+# Hostname
+sed -i "s|system.@system[-1].hostname='OpenWrt'|system.@system[-1].hostname='Xiaomi-AX3600'|g" package/base-files/files/bin/config_generate
+
+# Timezone and Zonename
+sed -i "s|system.@system\\[-1\\].timezone='UTC'|system.@system\\[-1\\].timezone='<+8>-8'\\
+                set system.@system[-1].zonename='Asia/Kuala Lumpur'|g" package/base-files/files/bin/config_generate
